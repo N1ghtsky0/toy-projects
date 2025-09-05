881eef6a0015caee7d13e82be4c788ade0fd6159
N1ghtsky0 <20korea01@gmail.com>
2025-09-05 14:03:56 +0900

    발표자 방송 시작 시 미디어 서버 연동

---
+++ webRtcDemo/src/main/resources/static/js/kurento.js
@@ -0,0 +1,103 @@
+const ws = new WebSocket(`ws://${location.host}/kurento`);
+const localVideoEl = document.getElementById('localVideo');
+
+let localStream;
+let pc;
+
+const pcConfig = {
+  iceServers: [
+    {
+      urls: 'stun:stun.l.google.com:19302'
+    }
+  ]
+};
+
+ws.onmessage = function (e) {
+  const jsonData = JSON.parse(e.data);
+  console.log(`[RECEIVE] ${JSON.stringify(jsonData)}`);
+
+  switch (jsonData.id) {
+    case 'icecandidate':
+      pc.addIceCandidate(new RTCIceCandidate(jsonData.result)).then(() => console.log('addIceCandidate'));
+      break;
+    case 'presenterAnswer':
+      if (jsonData.result === 'accepted') {
+        pc.setRemoteDescription(new RTCSessionDescription({sdp: jsonData.sdpAnswer, type: "answer"})).then(() => {
+          console.log('answer - setRemoteDescription');
+        });
+      }
+      break;
+    default:
+      console.log(jsonData);
+      break;
+  }
+}
+
+ws.onclose = function (e) {
+  console.log(`[CLOSE] ${e.code} ${e.reason}`);
+}
+
+ws.onerror = function (e) {
+  console.log(`[ERROR] ${e.message}`);
+}
+
+function createRTCPeerConnection(isPresenter = false) {
+  const pc = new RTCPeerConnection(pcConfig);
+
+  try {
+    pc.addEventListener('icecandidate', (e) => {
+      if (e.candidate) {
+        const obj = {
+          id: 'icecandidate',
+          result: e.candidate
+        }
+        send(obj);
+        console.log('ICE candidate');
+      }
+    });
+
+    pc.addEventListener('track', (e) => {
+      if (isPresenter) { return; }
+      localVideoEl.srcObject = e.streams[0];
+    })
+
+    pc.addEventListener('iceconnectionstatechange', (e) => {
+      console.log(`iceConnectionState: ${e.target.iceConnectionState}`)
+    });
+
+    if (localStream !== undefined) {
+      localStream.getTracks().forEach(track => {
+        pc.addTrack(track, localStream);
+      });
+    }
+
+    console.log('createRTCPeerConnection');
+  } catch (e) {
+    console.error(e);
+  }
+  return pc;
+}
+
+function send(msgObj) {
+  const jsonData = JSON.stringify(msgObj);
+  console.log(`[SEND] ${jsonData}`);
+  ws.send(jsonData);
+}
+
+async function startBroadcast() {
+  localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true } );
+  localVideoEl.srcObject = localStream;
+
+  pc = createRTCPeerConnection(true);
+  pc.addTransceiver('video', {direction: 'sendonly'});
+  pc.addTransceiver('audio', {direction: 'sendonly'});
+  pc.createOffer().then(offer => {
+    pc.setLocalDescription(offer).then(() => {
+      const obj = {
+        id: 'presenter',
+        sdpOffer: offer
+      }
+      send(obj);
+    });
+  })
+}
